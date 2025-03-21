84e2cc42c925e781582da9e3536609f389825ec8
Margarita Khodzka <Margarita.Khodzka@meredith.com>
2025-03-21 13:59:32 -0400

    reactive properties

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,9 +1,27 @@
 <script setup>
+import { useTodoListStore } from '@/stores/todoList';
+import { storeToRefs } from 'pinia';
+
+const store = useTodoListStore();
+const { todoList } = storeToRefs(store);
+const { toggleCompleted } = store;
 </script>
 
 <template>
-    <div></div>
+    <div>
+        <div v-for="todo in todoList" :key="todo.id" class="item">
+            <div class="content">
+                <!-- look at addTodo() action in store -->
+                <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
+                <!-- use unicode for the checkmark symbol -->
+                <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped>
+.completed {
+    text-decoration: line-through;
+}
 </style>
