4368453c1e11b2d285edce35d7e1d494fc2b0e1c
Margarita Khodzka <Margarita.Khodzka@meredith.com>
2025-03-21 14:19:21 -0400

    deleting state from the store

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,7 @@ import { storeToRefs } from 'pinia';
 
 const store = useTodoListStore();
 const { todoList } = storeToRefs(store);
-const { toggleCompleted } = store;
+const { toggleCompleted, deleteTodo } = store;
 </script>
 
 <template>
@@ -13,15 +13,33 @@ const { toggleCompleted } = store;
             <div class="content">
                 <!-- look at addTodo() action in store -->
                 <span :class="{ completed: todo.completed }">{{ todo.item }}</span>
-                <!-- use unicode for the checkmark symbol -->
-                <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+                <div>
+                    <!-- use unicode for symbols -->
+                    <span @click.stop="toggleCompleted(todo.id)">&#10004;</span>
+                    <span @click="deleteTodo(todo.id)" class="x">&#10006;</span>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
+span {
+  margin: 0 10px;
+  cursor: pointer;
+}
+.item {
+  display: flex;
+  justify-content: center;
+}
+.content {
+  display: flex;
+  font-size: 1.5em;
+  justify-content: space-between;
+  width: 80vw;
+  padding: 5px;
+}
 .completed {
-    text-decoration: line-through;
+  text-decoration: line-through;
 }
 </style>
